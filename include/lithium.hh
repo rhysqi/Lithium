6d56105dc085fddb54c43b968c34930ae0634d5d
rhysqi <rhysqi.akbar@gmail.com>
2023-11-05 23:52:44 +0700

    Update headers

--- include/lithium.hh
+++ include/lithium.hh
@@ -1,15 +1,5 @@
 #ifndef LITHIUM_HH
 #define LITHIUM_HH
 
-#include "component/button.h"
-#include "component/window.h"
-#include "component/menubar.h"
-#include "component/menu.h"
-
-#include "plugins/lsp.h"
-
-#include "temp/core.h"
-#include "temp/temp.h"
-#include "temp/handling.h"
 
 #endif
