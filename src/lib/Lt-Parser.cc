142654370ccc7681b45393defb14dcc5f0a26e53
rhysqi <rhysqi.akbar@gmail.com>
2023-11-18 00:45:21 +0700

    Update main files

--- src/lib/Lt-Parser.cc
+++ src/lib/Lt-Parser.cc
@@ -1,19 +1,53 @@
+/*
+ * BSD 3-Clause License
+ * 
+ * Copyright (c) 2023, Risky Akbar
+ * 
+ * Redistribution and use in source and binary forms, with or without
+ * modification, are permitted provided that the following conditions are met:
+ * 
+ * 1. Redistributions of source code must retain the above copyright notice, this
+ *    list of conditions and the following disclaimer.
+ * 
+ * 2. Redistributions in binary form must reproduce the above copyright notice,
+ *    this list of conditions and the following disclaimer in the documentation
+ *    and/or other materials provided with the distribution.
+ * 
+ * 3. Neither the name of the copyright holder nor the names of its
+ *    contributors may be used to endorse or promote products derived from
+ *    this software without specific prior written permission.
+ * 
+ * THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS "AS IS"
+ * AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT LIMITED TO, THE
+ * IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR A PARTICULAR PURPOSE ARE
+ * DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT HOLDER OR CONTRIBUTORS BE LIABLE
+ * FOR ANY DIRECT, INDIRECT, INCIDENTAL, SPECIAL, EXEMPLARY, OR CONSEQUENTIAL
+ * DAMAGES (INCLUDING, BUT NOT LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR
+ * SERVICES; LOSS OF USE, DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER
+ * CAUSED AND ON ANY THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY,
+ * OR TORT (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+ * OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE.
+ */
 
 #include "../../include/Lt-Core.hh"
 
+#include <stdarg.h>
+
 void Lt_Parser(){
-    
-}
-
-void Lt_ParseSym(){
 
 }
 
-void Lt_ParseArgs(){
+void Lt_ParseSym(const char* SYM){
 
 }
 
-void Lt_Parse_Read(){
+void Lt_ParseArgs(const char* Args){
 
+}
+
+void Lt_Parser_Read(const char* _Read, ...){
+
+    va_list args;
+    va_start(args, _Read);
 }
 
