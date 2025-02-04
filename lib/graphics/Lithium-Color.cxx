9a6bec563ccdc2839c55a66e48a32035da6f5bb2
rhysqi <rhysqi.akbar@gmail.com>
2025-02-04 10:36:02 +0700

    Add: Lib Color - Adding color conversion
    submit: rhysqi

---
+++ lib/graphics/Lithium-Color.cxx
@@ -0,0 +1,33 @@
+#if defined(_WIN32) || defined(_WIN64)
+
+#ifndef UNICODE
+#define UNICODE
+#endif /* UNICODE */
+
+#include "../../include/Lithium-Graphics.hh"
+
+#include <Windows.h>
+#include <wingdi.h>
+
+HBRUSH Lithium_Graphics::Color::CreateBrushFromHex(COLORREF hexColor) 
+{
+    BYTE r = (hexColor >> 16) & 0xFF;
+    BYTE g = (hexColor >> 8) & 0xFF;
+    BYTE b = hexColor & 0xFF;
+    return CreateSolidBrush(RGB(r, g, b));
+}
+
+HBRUSH Lithium_Graphics::Color::CreateBrushFromRGB(UINT8 Red, UINT8 Green, UINT8 Blue)
+{
+    return CreateSolidBrush(RGB(Red, Green, Blue));
+}
+
+COLORREF Lithium_Graphics::Color::Hex_RGB(COLORREF hexColor)
+{
+	BYTE R = (hexColor >> 16) & 0xff;
+	BYTE G = (hexColor >> 8) & 0xff;
+	BYTE B = (hexColor) & 0xff;
+	return RGB(R, G, B);
+}
+
+#endif /* defined(_WIN32) || defined(_WIN64) */
