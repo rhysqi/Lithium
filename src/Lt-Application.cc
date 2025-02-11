a179cce0017d5fa1561834a5b62c635c3ae4dc9a
rhysqi <rhysqi.akbar@gmail.com>
2025-02-12 00:41:52 +0700

    Add: Main files
    submit: rhysqi

---
+++ src/Lt-Application.cc
@@ -0,0 +1,13 @@
+#ifndef UNICODE
+#define UNICODE
+#endif /* UNICODE */
+
+#include <Windows.h>
+
+int WINAPI
+wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, LPWSTR lpCmdLine, int nShowCmd){
+
+    
+
+    return 0;
+}
