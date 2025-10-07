ea4dab73f823140cb14fc894b1e6651d7c86035f
rhysqi <rhysqi.akbar@gmail.com>
2025-10-08 06:07:08 +0700

    Update: Lithium-File.cxx
    submit: rhysqi

--- lib/system/Lithium-File.cxx
+++ lib/system/Lithium-File.cxx
@@ -1,4 +1,5 @@
 #include "../../include/Lithium-System.hh"
+#include <sys/types.h>
 
 #if defined(_WIN32) || defined(_WIN64)
 
@@ -7,11 +8,16 @@
 
 #ifdef __linux__
 
+#include <stdio.h>
 #include <sys/file.h>
 
-int Lithium_System::File::CreateFile(const char *filename)
+int Lithium_System::File::Create(const char *filename)
 {
-	
+	if (!fopen(filename, "w")) {
+		printf("Failed to create file: %s\n", filename);
+		return -1;
+	}
+
 	return 0;
 }
 
