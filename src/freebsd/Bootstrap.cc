a16f1a6280ee7c5df95615ca2ff03295ad3c8493
rhysqi <rhysqi.akbar@gmail.com>
2026-02-26 06:26:45 +0700

    Update: Bootstrap.cc - Adding ASLR check - Adding AntiDebugger check
    submit: rhysqi

--- src/freebsd/Bootstrap.cc
+++ src/freebsd/Bootstrap.cc
@@ -1,25 +1,35 @@
 #include "../../include/Lithium.hh"
 
+#include <sys/syscall.h>
+#include <unistd.h>
+
 void check_ASLR();
 void check_AntiDebugger();
+void Splashscreen();
 
 void Bootstrap()
 {
     #ifndef _DEBUG
-    #define _DEBUG
-    
     check_ASLR();
     check_AntiDebugger();
-    
     #endif /* _DEBUG */
+}
+
+[[noreturn]]
+static void terminate(int code)
+{
+    syscall(SYS_exit, code);
+    __builtin_unreachable();
 }
 
 void check_ASLR()
 {
-    
+    if (!Lithium_System::Util::ASLR())
+        terminate(Lithium_Types::Error::Bootstrap_ASLR_Required);
 }
 
 void check_AntiDebugger()
 {
-    
+    if (Lithium_System::Util::Debugger())
+        terminate(Lithium_Types::Bootstrap_Debugger_Detected);
 }
